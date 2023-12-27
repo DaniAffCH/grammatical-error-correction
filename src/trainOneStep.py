901147ef7ed64e1ff4023e4106f6bbaff0880631
DaniAffCH <danieleaffinita2000@gmail.com>
2023-12-27 20:28:51 +0100

    cleanup

--- src/trainOneStep.py
+++ src/trainOneStep.py
@@ -1,5 +1,3 @@
-import logging
-
 
 def trainOneStep(model, dataloader, optimizer, lr_scheduler, criterion, device):
     total_loss = 0
@@ -9,11 +7,9 @@ def trainOneStep(model, dataloader, optimizer, lr_scheduler, criterion, device):
         out = model(input["input_ids"].to(device), tgt_in["input_ids"].to(device),
                     input["attention_mask"].to(device), tgt_in["attention_mask"].to(device))
 
-        # BUG?????
         out = out.view(-1, out.size(2))
         gt = tgt_out["input_ids"].contiguous().view(-1).to(device)
 
-        # Not sure though
         loss = criterion(out, gt)
         loss.backward()
 
