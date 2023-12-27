8e6e430855d71ea97ce4f8198a5d32cff974cb3e
DaniAffCH <danieleaffinita2000@gmail.com>
2023-12-27 12:25:47 +0100

    add special tokens, fixed padding and batch ffw

--- jflegDataset.py
+++ jflegDataset.py
@@ -2,25 +2,31 @@ from torch.utils.data import Dataset
 import pandas as pd
 import numpy as np
 
+
 class JflegDataset(Dataset):
-    def __init__(self, path, tokenizer) -> None:
+    def __init__(self, path, tokenizer, max_len=64) -> None:
         super().__init__()
         self.data = pd.read_csv(path)
         self.tokenizer = tokenizer
         self._preprocess()
+        self.max_len = max_len
 
     def _preprocess(self):
-        self.data = self.data.groupby('input')['target'].agg(np.array).reset_index()
-        self.data["input"] = self.data["input"].str.replace(r'^grammar: ', '', regex=True) 
+        self.data = self.data.groupby(
+            'input')['target'].agg(np.array).reset_index()
+        self.data["input"] = self.data["input"].str.replace(
+            r'^grammar: ', '', regex=True)
 
     def _process_sequence(self, sequence):
-        result = self.tokenizer(sequence, return_tensors="pt", padding=True)
+        sequence = f"{self.tokenizer.bos_token} {sequence} {self.tokenizer.eos_token}"
+        result = self.tokenizer(sequence, return_tensors="pt",
+                                padding="max_length", truncation=True, max_length=self.max_len)
         result = {key: value.squeeze() for key, value in result.items()}
         return result
 
     def __len__(self):
         return self.data.size
-    
+
     def __getitem__(self, index):
         input = self.data.iloc[index]["input"]
         input = self._process_sequence(input)
@@ -29,6 +35,6 @@ class JflegDataset(Dataset):
         target = [self._process_sequence(s) for s in target_text_list]
 
         return input, target
-    
+
     def decode(self, embedding):
-        return self.tokenizer.decode(embedding)
+        return self.tokenizer.decode(embedding, skip_special_tokens=True)
